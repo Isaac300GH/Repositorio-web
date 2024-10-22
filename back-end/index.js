1f40425395cbd5354b5835cad7da5c241cd7ee96
Isaac300G <imgoisa2023@gmail.com>
2024-10-22 14:48:53 -0400

    express

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,12 @@
-console.log("hola mundo")
+/* const http = require('http');
+const port = 5000;
+
+const server = http.createServer((req, res) => res.end("HOLA :D"));
+server.listen(port, () => console.log("Funcionó")); */
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => res.send("HOLA :D"));
+app.get("/lapapaya", (req, res) => res.send("papasha"));
+app.listen(port, () => console.log("funcionó"));
